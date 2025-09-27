1ea9e376d5861901402101ab5f3d070f5a96bdbc
Sarah-Mathew2025 <sarahannamathew@gmail.com>
2025-09-27 15:19:32 -0700

    PedroPathing and MechStates
    PedroPathing and MechStates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MechState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MechState.java
@@ -1,4 +1,10 @@
 package org.firstinspires.ftc.teamcode.robot;
-
 public enum MechState {
+    IDLE,                   // 1
+    SHOOTING,               // 2
+    INTAKE,                 // 3
+    PURPLE_SORTING,         // 4
+    GREEN_SORTING,          // 5
+    APRILTAG,               // 6
+    HUMAN_STATE             // 7
 }
