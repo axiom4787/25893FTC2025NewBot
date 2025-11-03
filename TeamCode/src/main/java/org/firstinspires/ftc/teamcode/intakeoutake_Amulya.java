e75837d2741419caacb45f5db8fb5d9fa4023dd6
spati <ftcvelocity@gmail.com>
2025-11-02 18:36:44 -0600

    changed servos to dcmotors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeoutake_Amulya.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeoutake_Amulya.java
@@ -1,32 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
 //import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 //import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.hardware.CRServo;
+//import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 
 public class intakeoutake_Amulya {
 
     // INSTANTIATE MOTORS AND SERVOS
-    private CRServo intake;
-    private Servo claw;
+    private DcMotor intake;
+    //private Servo claw;
 
     private boolean clawOpen = true;
     private boolean lastBump = false;
 
     public void init(HardwareMap hardwareMap) {
-        intake = hardwareMap.get(CRServo.class, "intake");
-        claw = hardwareMap.get(Servo.class, "claw");
-        intake.setDirection(CRServo.Direction.REVERSE);
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        //claw = hardwareMap.get(DcMotor.class, "claw");
+        intake.setDirection(DcMotor.Direction.REVERSE);
         clawOpen = false;
         lastBump = false;
     }
 
-    public void GAMEPAD_INPUT_TOGGLE(Gamepad gamepad2, Telemetry telemetry) {
+  /*  public void GAMEPAD_INPUT_TOGGLE(Gamepad gamepad2, Telemetry telemetry) {
         // Toggle claw position when right_bumper is pressed
         telemetry.addData("lastBump - preOp", lastBump);
         if (gamepad2.right_bumper && !lastBump) {
@@ -44,9 +45,9 @@ public class intakeoutake_Amulya {
         telemetry.addData("lastBump - postOp", lastBump);
         telemetry.update();
     }
-
+*/
     public void GAMEPAD_INTAKE(Gamepad gamepad2, Telemetry telemetry) {
-        // Control intake servo with triggers
+        // Control intake motor with triggers
         if (gamepad2.right_trigger > 0.1) {
             intake.setPower(1.0);
         } else if (gamepad2.left_trigger > 0.1) {
