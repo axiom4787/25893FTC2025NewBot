8993df9995a8d86fe3de4a61df60349e51abc9a3
Hudson <hudson_meng@s.thevillageschool.com>
2025-11-03 22:32:54 -0600

    change getLaunchSpeed to getLaunchRPM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LauncherControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LauncherControl.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class LauncherControl {
+
+    private DcMotor launchMotor;
+    private double ticksPerRotation;
+
+    public void init(HardwareMap hardwareMap){
+        launchMotor = hardwareMap.get(DcMotor.class, "shooter");
+        launchMotor.setDirection(DcMotor.Direction.FORWARD);
+        launchMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRotation = 28;
+
+    }
+
+    public void launchBall(double power) {
+        launchMotor.setPower(power);
+    }
+
+    public double getlaunchRPM() {
+        return launchMotor.getCurrentPosition() / ticksPerRotation * 60;
+    }
 }
