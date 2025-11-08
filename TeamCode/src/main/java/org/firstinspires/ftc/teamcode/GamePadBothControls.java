11f9e6b946e4590814c17d20ae6e577a2c422802
TopProjectsCreator <ishaanbans@gmail.com>
2025-11-08 12:36:50 -0800

    Update GamePadBothControls.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadBothControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadBothControls.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class GamePadTester extends LinearOpMode {
+public class GamePadBothControls extends LinearOpMode {
 
     @Override
-    public void GamePadBothControls() {
+    public void runOpMode() {
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
