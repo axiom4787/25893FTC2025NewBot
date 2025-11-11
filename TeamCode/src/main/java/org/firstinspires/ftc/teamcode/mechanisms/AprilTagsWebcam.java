adb0953bf9a3e5bebd69b20f954dc955a51ad877
Bridger <Bdsmadlu@gmail.com>
2025-11-10 20:13:52 -0600

    Fixed detection to be detectedID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagsWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagsWebcam.java
@@ -60,13 +60,13 @@ public class AprilTagsWebcam {
             return;
         }
         if (detectedId.metadata != null) {
-            telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detectedId.id, detectedId.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detectedId.ftcPose.x, detectedId.ftcPose.y, detectedId.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detectedId.ftcPose.pitch, detectedId.ftcPose.roll, detectedId.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detectedId.ftcPose.range, detectedId.ftcPose.bearing, detectedId.ftcPose.elevation));
         } else {
-            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detectedId.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detectedId.center.x, detectedId.center.y));
         }
     }
     public AprilTagDetection getTagBySpecificId(int id) {
