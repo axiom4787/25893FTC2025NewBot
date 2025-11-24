bcc72057a7f8a01b29813f3662c87ca37bcd36f8
MrChips10 <jason.fogiel@outlook.com>
2025-11-24 14:09:54 -0500

    Updated The Fidget Tech
    changed the Fidget Tech code so it now snaps to its positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheKeep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheKeep.java
@@ -8,24 +8,33 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="The Keep",group="The Keep")
 public class TheKeep extends OpMode {
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-    private DcMotor shooter = null;
+    private DcMotor FrontL = null;
+    private DcMotor FrontR = null;
+    private DcMotor BackL = null;
+    private DcMotor BackR = null;
+    private DcMotor Shooter = null;
+
     private Servo ballEjector = null;
     private Servo spinIndexer = null;
-    private double spinPosition = 0.6;
+    private CRServo intake = null;
+    private int number = 13;
+    private double[] spinPosition = {0.3,0.6,0.1,0.14,0.17,0.21,0.25,0.28,0.32,0.36,0.4,0.43,0.47,0.51,0.55,0.58,0.62,0.66,0.7,0.73,0.77,0.8,0.83,0.87,0.91,0.94,0.98};
     @Override
     public void init() {
 
-        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
-        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+        FrontL = hardwareMap.get(DcMotor.class,"FrontL");
+        FrontR = hardwareMap.get(DcMotor.class, "FrontR");
+        BackL = hardwareMap.get(DcMotor.class, "BackL");
+        BackR = hardwareMap.get(DcMotor.class, "BackR");
+        Shooter = hardwareMap.get(DcMotor.class,"Shooter");
+        intake = hardwareMap.get(CRServo.class,"intake");
+        FrontL.setDirection(DcMotorSimple.Direction.FORWARD);
+        FrontR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackL.setDirection(DcMotorSimple.Direction.FORWARD);
+        BackR.setDirection(DcMotorSimple.Direction.REVERSE);
         ballEjector = hardwareMap.get(Servo.class, "ballEjector");
         spinIndexer = hardwareMap.get(Servo.class, "spinIndexer");
-        shooter = hardwareMap.get(DcMotor.class, "shooter");
         //These lines setup the motors and servos
-
-        leftDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
         //These lines will set the direction of the motors so 1 power is forward
 
     } // This initializes all the variables and motors
@@ -33,28 +42,31 @@ public class TheKeep extends OpMode {
     @Override
     public void loop() {
 
-        leftDrive.setPower((gamepad1.left_stick_y-gamepad1.left_stick_x)/2);
-        rightDrive.setPower((gamepad1.left_stick_y+gamepad1.left_stick_x)/2);
+        FrontL.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x)/2);
+        FrontR.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x)/2);
+        BackL.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x)/2);
+        BackR.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x)/2);
         // These lines control the wheel motors
 
-        if (gamepad1.rightBumperWasPressed())  spinPosition = spinPosition + 0.2;
-        if (gamepad1.leftBumperWasPressed()) spinPosition = spinPosition - 0.2;
-        if (spinPosition>1) spinPosition = 1;
-        if (spinPosition<0) spinPosition = 0;
-        spinIndexer.setPosition(spinPosition);
+        if (gamepad1.leftBumperWasPressed() && number != 0) number -= 1;
+        if (gamepad1.rightBumperWasPressed() && number != 26) number += 1;
+        spinIndexer.setPosition(spinPosition[number]);
+        if (gamepad1.circle) {
+            Shooter.setPower(1);
+        } else Shooter.setPower(0);
+        if (gamepad1.triangle && number % 2 == 1) {
+            ballEjector.setPosition(.3);
+        } else ballEjector.setPosition(0);
+        if (gamepad1.square) {
+            intake.setPower(1);
+        } else intake.setPower(0);
         // These lines control the spin indexer
-
-        if (gamepad1.right_stick_y>0) ballEjector.setPosition(.2);
-        if (gamepad1.right_stick_y<=0) ballEjector.setPosition(0);
-
         // This controls the ball ejector
-
-        if (gamepad1.dpad_up) shooter.setPower(1);
-        if (gamepad1.dpad_down) shooter.setPower(0);
         // These lines control the shooting flywheel
-        telemetry.addData("SpinIndexer Position", spinPosition);
+        telemetry.addData("SpinIndexer Position", spinPosition[number]);
         telemetry.update();
 
     } // This section controls all the motors using the remote control
 
 }
+
