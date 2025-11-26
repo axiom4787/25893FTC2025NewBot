8897a0cbdf9a496872b65baa9e6fed4384484620
tango8 <ethan.b.tang@gmail.com>
2025-11-26 14:00:51 -0500

    actuator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Actuator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Actuator.java
@@ -1,25 +1,40 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Actuator {
-    private final int DOWN = 0; // flush with the floor of platform
-    private final int UP = 180; // raised to push it into the flywheel
     private final double DOWN = 0.0; // flush with the floor of platform
     private final double UP = .27; // raised to push it into the flywheel
 
     private boolean activated;
 
-    @ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Actuator.java:16 @ public class Actuator {
+    private final SimpleServo servo;
+
+    public Actuator(HardwareMap hardwareMap) {
+        servo = new SimpleServo(hardwareMap, "actuator", 0, 360);
     }
 
     public void down() {
-        servo.turnToAngle(DOWN);
         servo.setPosition(DOWN);
         activated = false;
     }
 
     public void up() {
-        servo.turnToAngle(UP);
         servo.setPosition(UP);
         activated = true;
     }
+
+    public boolean isActivated()
+    {
+        return activated;
+    }
+
+    public void set(boolean activate)
+    {
+        if (activate)
+            up();
+        else
+            down();
+    }
+}
