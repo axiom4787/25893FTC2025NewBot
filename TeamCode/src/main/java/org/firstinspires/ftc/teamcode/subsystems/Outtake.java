31f0a4ba257f97170d1e53d2cbace2107660877a
tango8 <ethan.b.tang@gmail.com>
2025-11-26 14:04:57 -0500

    outtake new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Outtake.java
@@ -1,30 +1,17 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Outtake {
     private final MotorEx motor;
-    private double rpm;
-    double lastTime;
-    
-    public static class Params
-    {
-        public static final double DISTANCE_TO_RPM_COEFF = 0.8;
-    }
+    private double power;
+    final double TICKS_PER_REV = 112.0;
 
     public Outtake(HardwareMap hardwareMap)
     {
         motor = new MotorEx(hardwareMap, "outtake");
-        // set the run mode
-        motor.setRunMode(Motor.RunMode.VelocityControl);
-
-        // set and get the coefficients
-        motor.setVeloCoefficients(0.05, 0.01, 0.31);
-        motor.setFeedforwardCoefficients(0.92, 0.47);
-        motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
-        lastTime = System.nanoTime();
+        power = -1;
     }
 
     public void stop()
@@ -34,47 +21,19 @@ public class Outtake {
 
     public void run()
     {
-        motor.setVelocity(rpmToTicksPerSecond(rpm));
+        motor.set(power);
     }
 
-    public void setRPM(double newPower)
+    public void setPower(double newPower)
     {
-        rpm = newPower;
+        power = newPower;
+        motor.set(power);
     }
 
-    public double getRPM()
+    public double getPower()
     {
-        return rpm;
+        return power;
     }
 
-    public double getMeasuredRPM()
-    {
-        double ticksPerSecond = motor.get();
-        double ticksPerRev = motor.getCPR();
-        double revsPerSecond = ticksPerSecond / ticksPerRev;
-        return revsPerSecond * 60.0;
-    }
-
-    public double rpmToTicksPerSecond(double rpm) {
-        double ticksPerRev = motor.getCPR(); // counts per revolution
-        return (rpm / 60.0) * ticksPerRev;
-    }
-
-    public void setOuttakeVelocityFromDistance(double distanceInInches) {
-        // Simple model: farther target = higher velocity
-        // You’ll replace this with a better fit (quadratic or regression later)
-        double targetRPM = Params.DISTANCE_TO_RPM_COEFF * Math.sqrt(distanceInInches);
-
-        // Convert to ticks/sec
-        double targetVelocityTicks = rpmToTicksPerSecond(targetRPM);
-
-        // Set the velocity target
-        motor.setVelocity(targetVelocityTicks);
-
-        // if we wanna debug later
-        //telemetry.addData("Target Distance (in)", distanceInInches);
-        //telemetry.addData("Target RPM", targetRPM);
-        //telemetry.addData("Target Velocity (tps)", targetVelocityTicks);
-    }
     //TODO: Make algorithm to determine power needed based on distance
 }
