581c3b08bc0140150de430f220af2491dfbd6f2b
andrewcodes28 <180860358+andrewcodes28@users.noreply.github.com>
2025-12-01 15:33:53 -0500

    fix issue where the blue color wouldn't be checked, increase readability

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ColorSensorSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ColorSensorSystem.java
@@ -17,32 +17,26 @@ public class ColorSensorSystem {
 
     // checks if the int provided is less than a max and a min calculated using a starting int and a tolerance percent
     private boolean checkRange(int num, int origin, double tolerance) {
-        boolean valid = false;
         int min = (int)(origin * (1 - tolerance));
         int max = (int)(origin * (1 + tolerance));
-        if (num > min && num < max) {
-            valid = true;
-        }
-        return valid;
+        return num > min && num < max;
     }
 
-    // checks if the camera sees purple or green and returns -1 (neither found) 0 (purple found) 1 (green found)
     public Indexer.ArtifactColor getColor() {
-        Indexer.ArtifactColor foundColor = Indexer.ArtifactColor.unknown;
-        int[] rgb = {colorSensor.red(),colorSensor.green(),colorSensor.blue()};
+        int[] rgb = {colorSensor.red(), colorSensor.green(), colorSensor.blue()};
         boolean foundPurple = true;
-        for (int i = 0; i < 2; i++) {
-            foundPurple = (foundPurple && checkRange(rgb[i],PURPLE_RGB[i],PURPLE_TOLERANCE));
+        for (int i = 0; i < 3; i++) {
+            foundPurple &&= checkRange(rgb[i], PURPLE_RGB[i], PURPLE_TOLERANCE);
         }
         if (foundPurple) {
-            foundColor = Indexer.ArtifactColor.purple;
+            return Indexer.ArtifactColor.purple;
         } else {
             boolean foundGreen = true;
-            for (int i = 0; i < 2; i++) {
-                foundGreen = (foundGreen && checkRange(rgb[i],GREEN_RGB[i],GREEN_TOLERANCE));
+            for (int i = 0; i < 3; i++) {
+                foundGreen &&= checkRange(rgb[i], GREEN_RGB[i], GREEN_TOLERANCE);
             }
-            if (foundGreen) foundColor = Indexer.ArtifactColor.green;
+            if (foundGreen) return Indexer.ArtifactColor.green;
         }
-        return foundColor;
+        return Indexer.ArtifactColor.unknown;
     }
 }
